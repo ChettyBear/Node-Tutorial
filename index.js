5cd87e4bc26a345da32f7dd87b67d53c4dc6bf55
ChettyBear <matthew.j.chetty@gmail.com>
2024-04-24 16:06:35 +0200

    Avocado ting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const fs = require('fs'); 
 
-const hello ='Hello world';
-console.log(hello) ;
+const textIn = fs.readFileSync('./1-node-farm/starter/txt/input.txt', 'utf-8');
+console.log(textIn);
