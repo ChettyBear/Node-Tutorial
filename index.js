ffb1572f99394a010163562139e929a67db53fbf
ChettyBear <matthew.j.chetty@gmail.com>
2024-04-25 11:31:55 +0200

    Made Output

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const fs = require('fs');
 
 const textIn = fs.readFileSync('./1-node-farm/starter/txt/input.txt', 'utf-8');
 console.log(textIn);
+
+const textOut = `this is waht we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./1-node-farm/starter/txt/output.txt', textOut);
+console.log('File wirtten');
