41144b629c537f16eaae3e02a5b3e60585c9954d
ChettyBear <matthew.j.chetty@gmail.com>
2024-04-25 16:43:29 +0200

    made asyncronous functions

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-const fs = require('fs'); 
+const fs = require("fs");
 
-const textIn = fs.readFileSync('./1-node-farm/starter/txt/input.txt', 'utf-8');
+// Block, synchronous way
+const textIn = fs.readFileSync("./1-node-farm/starter/txt/input.txt", "utf-8");
 console.log(textIn);
 
 const textOut = `this is waht we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./1-node-farm/starter/txt/output.txt', textOut);
-console.log('File wirtten');
+fs.writeFileSync("./1-node-farm/starter/txt/output.txt", textOut);
+console.log("File wirtten");
+
+// Non-blocking, asynchronous way.
+fs.readFile("./1-node-farm/starter/txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR!");
+  fs.readFile(
+    `./1-node-farm/starter/txt/${data1}.txt`,
+    "utf-8",
+    (err, data2) => {
+      fs.readFile(
+        "./1-node-farm/starter/txt/append.txt",
+        "utf-8",
+        (err, data3) => {
+          console.log(data3);
+
+          fs.writeFile(
+            "./1-node-farm/txt/final.txt",
+            `${data2}\n${data3}`,
+            "utf-8",
+            (err) => {
+              console.log("Your file ahs been written!");
+            }
+          );
+        }
+      );
+    }
+  );
+});
+console.log("Will read file!");
