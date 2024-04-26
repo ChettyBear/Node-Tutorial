44dc9317302e11bf5305139fd04fe498b698e815
ChettyBear <matthew.j.chetty@gmail.com>
2024-04-26 16:41:50 +0200

    Made simple API and page routing

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const http = require("http");
+const { json } = require("stream/consumers");
+const url = require("url");
 
 //////////////////////////////////////////////////////////////////////////////////////
 //FILES
@@ -42,8 +44,30 @@ const http = require("http");
 
 //////////////////////////////////////////////////////////////////////////////////////
 //SERVER
+
+const data = fs.readFileSync(
+  `${__dirname}/1-node-farm/starter/dev-data/data.json`,
+  "utf-8"
+);
+const DataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW!");
+  } else if (pathName === "/product") {
+    res.end("his is the PRODUCT!");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "Hello-world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
