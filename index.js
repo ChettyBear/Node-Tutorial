19913d4466f2c385250ceadb3f5f02ecc37933dd
ChettyBear <matthew.j.chetty@gmail.com>
2024-05-02 06:34:24 +0200

    a lot of changes using npms

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const { json } = require("stream/consumers");
 const url = require("url");
+const replaceTemplate = require("./1-node-farm/modules/replaceTemplate");
+const slugify = require("slugify");
 
 //////////////////////////////////////////////////////////////////////////////////////
 //FILES
@@ -44,22 +46,6 @@ const url = require("url");
 
 //////////////////////////////////////////////////////////////////////////////////////
 //SERVER
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%ORGANIC%}/g, product.organic);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
-
 const tempOverview = fs.readFileSync(
   `${__dirname}/1-node-farm/starter/templates/template-overview.html`,
   "utf-8"
@@ -79,11 +65,14 @@ const data = fs.readFileSync(
 );
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   //OVerview
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -93,11 +82,14 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     //Product
-  } else if (pathName === "/product") {
-    res.end("his is the PRODUCT!");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     //API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "content-type": "application/json" });
     res.end(data);
 
